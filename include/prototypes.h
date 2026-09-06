007a431588a54319959853e3c184360e5716367f
Andrew Via <andrew.n.via@gmail.com>
2026-09-06 00:55:49 -0400

    Properly included stack trace within prototypes.h

--- include/prototypes.h
+++ include/prototypes.h
@@ -629,6 +629,9 @@ extern 	pid32 	setsystem(pid32);
 /* in file fork.c */
 extern	pid32	fork(void);
 
+/* in file stacktrace.c */
+extern	syscall	stacktrace(int);
+
 /* NETWORK BYTE ORDER CONVERSION NOT NEEDED ON A BIG-ENDIAN COMPUTER */
 #define	htons(x)   ( ( 0xff & ((x)>>8) ) | ( (0xff & (x)) << 8 ) )
 #define	htonl(x)   ( (((x)>>24) & 0x000000ff) | (((x)>> 8) & 0x0000ff00) | \
